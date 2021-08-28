f690a1eef91d015542363c75d57c8dfc6b3f74f5
danilmv <43599357+danilmv@users.noreply.github.com>
2021-08-28 13:05:31 +0300

    L2: Fragment: settings

--- app/src/main/java/com/andriod/nasa/MainActivity.kt
+++ app/src/main/java/com/andriod/nasa/MainActivity.kt
@@ -2,14 +2,20 @@ package com.andriod.nasa
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import com.andriod.nasa.databinding.ActivityMainBinding
 import com.andriod.nasa.fragment.PictureOfTheDayFragment
+import com.andriod.nasa.fragment.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val fragmentPictureOfTheDay by lazy { PictureOfTheDayFragment() }
+    private val fragmentSettings by lazy { SettingsFragment() }
+
+    private val bottomView: BottomNavigationView by lazy { binding.bottomView }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +25,39 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, fragmentPictureOfTheDay)
             .commit()
+
+        prepareBottomNavigationView()
     }
+
+    private fun prepareBottomNavigationView() {
+        bottomView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.menu_bottom_item_picture->{
+                    showPicture()
+                }
+                R.id.menu_bottom_item_settings->{
+                    showSettings()
+                }
+                else->{ return@setOnItemSelectedListener false}
+            }
+            true
+        }
+    }
+
+    private fun showPicture() {
+        supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, fragmentPictureOfTheDay)
+            .commit()
+    }
+
+    private fun showSettings() {
+        supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, fragmentSettings)
+            .commit()
+    }
+
 }
---
+++ app/src/main/java/com/andriod/nasa/fragment/SettingsFragment.kt
@@ -0,0 +1,22 @@
+package com.andriod.nasa.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.andriod.nasa.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding: FragmentSettingsBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentSettingsBinding.inflate(inflater)
+        return binding.root
+    }
+}
